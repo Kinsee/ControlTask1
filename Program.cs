bbe9219eeae2176ac350c8900770e58cea760557
Mar'ya <kinsv@mail.ru>
2022-07-25 20:17:14 +0300

    функция print

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Введите массив:");
+string [] input=new String[3] {"Годный", "бывалый", "гудеж"};
+string [] result=new String[3];
+
+void Print (string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+
+    }
+    Console.WriteLine();
+}
+
+Print (input);
+
+// for (int i = 0; i < input.Length; i++)
+// {
+//     Console.Write(input[i]);
+    
+// }
+
+
+
+// for (int j = 0; j < result.Length; j++)
+// {
+//     Console.WriteLine("Строки менее 3х символов "+ result[j]);
+// }
+
+
+// Console.Write(input);
